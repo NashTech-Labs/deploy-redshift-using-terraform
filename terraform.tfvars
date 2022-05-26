bdc5cce818e84697177eea5868eed43e37865601
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-05-27 01:55:07 +0530

    Adding  terraform script

---
+++ terraform.tfvars
@@ -0,0 +1,22 @@
+aws_region = "us-west-2"
+
+rs_cluster_identifier = "studio9-cluster"
+
+rs_database_name = "database_cluster"
+
+rs_master_username = "studio9"
+
+rs_master_pass = "Admin123"
+
+rs_nodetype = "dc2.large"
+
+rs_cluster_type = "single-node"
+
+vpc_cidr = "10.0.0.0/16"
+
+redshift_subnet_cidr_1 = "10.0.1.0/24"
+
+redshift_subnet_cidr_2 = "10.0.2.0/24"
+
+profile = "terraform"
+
---
+++ variable.tf
@@ -0,0 +1,50 @@
+variable "aws_region" {
+
+}
+
+variable "rs_cluster_identifier" {
+
+}
+
+variable "rs_database_name" {
+
+}
+
+variable "rs_master_username" {
+
+}
+
+variable "rs_master_pass" {
+
+}
+
+variable "rs_nodetype" {
+
+}
+
+variable "rs_cluster_type" {
+
+}
+
+variable "vpc_cidr" {
+
+}
+
+variable "redshift_subnet_cidr_1" {
+
+}
+
+variable "redshift_subnet_cidr_2" {
+
+}
+variable "profile" {
+    description = "AWS credentials profile you want to use"
+}
+
+# variable "access_key" {
+
+# }
+
+# variable "secret_key" {
+
+# }
